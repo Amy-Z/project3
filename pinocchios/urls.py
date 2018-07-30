14b033dec3e2854094d8bc38a19644348ca30dfa
Amy Zhou <zhouamy77@gmail.com>
2018-07-30 16:19:41 +0000

    working on login/out

--- pinocchios/urls.py
+++ pinocchios/urls.py
@@ -23,8 +23,9 @@ from django.contrib.auth import views as auth_views
 from orders import views as orders_views
 
 urlpatterns = [
-    url(r'^$', orders_views.home, name='home'),
+    # path(r'^$', orders_views.home, name='home'),
+    path("admin/", admin.site.urls),
+    path("signup/", orders_views.signup, name='signup'),
     # url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     # url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
-    url(r'^signup/', orders_views.signup, name='signup'),
 ]
