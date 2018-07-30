8a12c225068e8aea9e8d7b6178e18991f0325ae7
Amy Zhou <zhouamy77@gmail.com>
2018-07-30 00:37:46 +0000

    start project3

---
+++ orders/models.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# class Login(models.Model):
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+
+# class Pizza
+
